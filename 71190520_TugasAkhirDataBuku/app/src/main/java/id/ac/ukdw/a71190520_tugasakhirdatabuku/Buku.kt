b8d52e2904e17ef863f4f118ae608349fc7b0601
JulianSitorus <julian.marsangap@ti.ukdw.ac.id>
2022-06-25 00:02:51 +0700

    Final

---
+++ 71190520_TugasAkhirDataBuku/app/src/main/java/id/ac/ukdw/a71190520_tugasakhirdatabuku/Buku.kt
@@ -0,0 +1,4 @@
+package id.ac.ukdw.a71190520_tugasakhirdatabuku
+
+data class Buku(val judulBuku: String, val namaPenulis: String, val namaPenerbit: String, val tahunTerbit: String, val jumlahHalaman : String) {
+}
